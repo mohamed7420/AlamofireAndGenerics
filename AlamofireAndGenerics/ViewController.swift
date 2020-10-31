aacdba6c9d0de1fd878ffda6062e13288234ed5f
Mohamed osama <mohamed.osama9812@gmail.com>
2020-10-31 10:49:43 -0700

    Initial Commit

---
+++ AlamofireAndGenerics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireAndGenerics
+//
+//  Created by Mohamed on 10/31/20.
+//  Copyright © 2020 Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
